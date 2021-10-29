d94af3673e2ca84164869c31ecf228095c9c4f6f
Shunki Takami <tkmnet@users.noreply.github.com>
2021-10-29 19:43:48 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+sudo apt -y update
+sudo apt -y install openbox lxqt
+
+
+cat << EOF > ~/.xsession
+openbox-session &
+startlxqt &
+EOF
+
+
+cat << EOF | sudo tee /usr/local/bin/rdpserver
+#!/bin/sh
+sudo xrdp-sesman
+sudo xrdp
+ip a
+EOF
+sudo chmod a+x /usr/local/bin/rdpserver
+
+echo
+echo #########################
+echo '"rdpserver" is available'
